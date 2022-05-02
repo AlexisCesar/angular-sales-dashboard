40fac6fc36dc7776c6c3b989347c8a011b95111f
Alexis Cesar <alexis.almeida@programmers.com.br>
2022-05-02 09:10:00 -0300

    First commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SalesDashboardComponent } from './modules/sales-dashboard/sales-dashboard.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: SalesDashboardComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/modules/sales-dashboard/sales-dashboard.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { SalesDashboardComponent } from './sales-dashboard.component';
+
+
+
+@NgModule({
+  declarations: [
+    SalesDashboardComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class SalesDashboardModule { }
